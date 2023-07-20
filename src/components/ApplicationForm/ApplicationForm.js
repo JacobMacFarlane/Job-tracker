0dcaad82ad4ac8c397b66cbc402b71f321c849c1
Jacob MacFarlane <jmacfarlane22@turing.edu>
2023-07-20 09:10:57 -0700

    shows the applicationform complete" "

--- src/components/ApplicationForm/ApplicationForm.js
+++ src/components/ApplicationForm/ApplicationForm.js
@@ -1,8 +1,93 @@
 import React, {useState} from "react";
 
 const ApplicationForm = ({ onSubmit }) => {
+    const [companyName, setCompanyName] = useState('')
+    const [role,setRole] = useState('')
+    const [date,setDate] = useState('')
+    const [linkedInContact,setLinkedInContact] = useState('')
+
+    const resetInputs = () => {
+        setCompanyName('')
+        setRole('')
+        setDate('')
+        setLinkedInContact('')
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        const applicationData = {
+            companyName,
+            role,
+            date,
+            linkedInContact,
+            comments: ''
+        }
+        onSubmit(applicationData)
+        resetInputs()
+    }
     return (
-        <div></div>
+        <form onSubmit={handleSubmit} className="bg-gray-100 p-4 rounded-lg">
+            <div className="mb-4">
+                <label classname="block text-gray-700 text-sm font-bold mb-2" htmlFor="companyName">
+                    Company Name
+                </label>
+                <input 
+                type="text" 
+                name="companyName" 
+                id="companyName" 
+                value={companyName} 
+                onChange={(e) => setCompanyName(e.target.value)} 
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                required
+                />
+            </div>
+            <div className="mb-4">
+                <label classname="block text-gray-700 text-sm font-bold mb-2" htmlFor="role">
+                    Role
+                </label>
+                <input 
+                type="text" 
+                name="role" 
+                id="role" 
+                value={role} 
+                onChange={(e) => setRole(e.target.value)} 
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                required
+                />
+            </div>
+            <div className="mb-4">
+                <label classname="block text-gray-700 text-sm font-bold mb-2" htmlFor="date">
+                    Company Name
+                </label>
+                <input 
+                type="text" 
+                name="date" 
+                id="date" 
+                value={date} 
+                onChange={(e) => setDate(e.target.value)} 
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                required
+                />
+            </div>
+            <div className="mb-4">
+                <label classname="block text-gray-700 text-sm font-bold mb-2" htmlFor="linkedInContact">
+                    Company Name
+                </label>
+                <input 
+                type="text" 
+                name="linkedInContact" 
+                id="linkedInContact" 
+                value={linkedInContact} 
+                onChange={(e) => setLinkedInContact(e.target.value)} 
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                required
+                />
+            </div>
+            <button type="submit" className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded mt-2">
+                Submit
+            </button>
+        </form>
     )
 }
 
